3d91402cab5b14c5f2cd57a21a42ac4ff3982bad
Michael J. Klein <lambdamichael@gmail.com>
2023-12-13 14:05:08 -0500

    move dist to docs

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   output: {
     filename: '[name]-[contenthash]-bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   optimization: {
     splitChunks: {
